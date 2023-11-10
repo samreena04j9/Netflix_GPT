57fc2994f34485eecf4792515f89b21e0fdb7d4e
samreen <samreen9815@gmail.com>
2023-11-10 16:20:38 +0530

    TMDB API data Fetch && built MainContainer

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const USER_AVATAR =
+  "https://occ-0-3216-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABY20DrC9-11ewwAs6nfEgb1vrORxRPP9IGmlW1WtKuaLIz8VxCx5NryzDK3_ez064IsBGdXjVUT59G5IRuFdqZlCJCneepU.png?r=229";
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjU2NzViZmYzYjcxNTYzOWU4ZGZhOTFmYWIxNWQ5MSIsInN1YiI6IjY1M2EyNDhlOGEwZTliMDBhZDIxMzAyNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z7ELylsqUiD8JidHKBMYGGAdJu6HhVtAYXMpv5hPbgs",
+  },
+};
